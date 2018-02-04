d4230bd433a3dccfa0d47f6b1c7663a2b9145a2a
Yaroslav Klymko <t3hnar@gmail.com>
2018-02-04 19:17:11 +0100

    update dependencies

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,13 +3,13 @@ import sbt._
 object Dependencies {
 
   object Akka {
-    private val version = "2.5.6"
+    private val version = "2.5.9"
 
     val Actor = "com.typesafe.akka" %% "akka-actor" % version
     val Persistence = "com.typesafe.akka" %% "akka-persistence" % version
     val TestKit = "com.typesafe.akka" %% "akka-testkit" % version % Test
   }
 
-  lazy val ScalaTest = "org.scalatest" %% "scalatest" % "3.0.4" % Test
+  lazy val ScalaTest = "org.scalatest" %% "scalatest" % "3.0.5" % Test
   lazy val PersistenceInmemory = "com.github.dnvriend" %% "akka-persistence-inmemory" % "2.5.1.1" % Test
 }
