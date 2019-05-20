cf53edcf59e80d2e6920743cd1c8fb41bb9615b1
Yaroslav Klymko <t3hnar@gmail.com>
2019-05-20 14:12:00 +0200

    update dependencies

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,29 +3,24 @@ import sbt._
 object Dependencies {
 
   object Akka {
-    private val version = "2.5.21"
-
-    val actor               = "com.typesafe.akka" %% "akka-actor" % version
-    val persistence         = "com.typesafe.akka" %% "akka-persistence" % version
-    val testkit             = "com.typesafe.akka" %% "akka-testkit" % version
-    val stream              = "com.typesafe.akka" %% "akka-stream" % version
+    private val version = "2.5.22"
+    val actor               = "com.typesafe.akka" %% "akka-actor"             % version
+    val persistence         = "com.typesafe.akka" %% "akka-persistence"       % version
+    val testkit             = "com.typesafe.akka" %% "akka-testkit"           % version
+    val stream              = "com.typesafe.akka" %% "akka-stream"            % version
     val `persistence-query` = "com.typesafe.akka" %% "akka-persistence-query" % version
-    val slf4j               = "com.typesafe.akka" %% "akka-slf4j" % version
+    val slf4j               = "com.typesafe.akka" %% "akka-slf4j"             % version
   }
 
   object Slf4j {
     private val version = "1.7.26"
-    val api                 = "org.slf4j" % "slf4j-api" % version
+    val api                 = "org.slf4j" % "slf4j-api"        % version
     val `log4j-over-slf4j`  = "org.slf4j" % "log4j-over-slf4j" % version
   }
 
-  val scalatest = "org.scalatest" %% "scalatest" % "3.0.6" % Test
-
+  val scalatest                   = "org.scalatest"       %% "scalatest"                 % "3.0.7"
+  val `logback-classic`           = "ch.qos.logback"       % "logback-classic"           % "1.2.3"
   val `akka-persistence-inmemory` = "com.github.dnvriend" %% "akka-persistence-inmemory" % "2.5.15.1"
-
-  val `executor-tools` = "com.evolutiongaming" %% "executor-tools" % "1.0.1"
-
-  val nel = "com.evolutiongaming" %% "nel" % "1.3.3"
-
-  val `logback-classic` = "ch.qos.logback" % "logback-classic" % "1.2.3"
+  val `executor-tools`            = "com.evolutiongaming" %% "executor-tools"            % "1.0.1"
+  val nel                         = "com.evolutiongaming" %% "nel"                       % "1.3.3"
 }
