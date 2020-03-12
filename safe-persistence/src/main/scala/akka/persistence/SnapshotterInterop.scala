dbdd569ef15dcfb0962d7b8eaad34fce849a10f5
Yaroslav Klymko <t3hnar@gmail.com>
2020-03-12 13:53:36 +0200

    reformat

--- safe-persistence/src/main/scala/akka/persistence/SnapshotterInterop.scala
+++ safe-persistence/src/main/scala/akka/persistence/SnapshotterInterop.scala
@@ -6,20 +6,19 @@ import com.evolutiongaming.safeakka.persistence.SeqNr
 
 object SnapshotterInterop {
 
-  def apply[A](snapshotter: Snapshotter): safeakka.persistence.Snapshotter[A] = {
+  def apply[A](
+    snapshotter: Snapshotter
+  ): safeakka.persistence.Snapshotter[A] = new safeakka.persistence.Snapshotter[A] {
 
-    new safeakka.persistence.Snapshotter[A] {
+    def save(snapshot: A) = snapshotter.saveSnapshot(snapshot)
 
-      def save(snapshot: A) = snapshotter.saveSnapshot(snapshot)
-
-      def save(seqNr: SeqNr, snapshot: A) = {
-        val saveSnapshot = SaveSnapshot(SnapshotMetadata(snapshotter.snapshotterId, seqNr), snapshot)
-        snapshotter.snapshotStore.tell(saveSnapshot, snapshotter.self)
-      }
-
-      def delete(seqNr: SeqNr) = snapshotter.deleteSnapshot(seqNr)
-
-      def delete(criteria: SnapshotSelectionCriteria) = snapshotter.deleteSnapshots(criteria)
+    def save(seqNr: SeqNr, snapshot: A) = {
+      val saveSnapshot = SaveSnapshot(SnapshotMetadata(snapshotter.snapshotterId, seqNr), snapshot)
+      snapshotter.snapshotStore.tell(saveSnapshot, snapshotter.self)
     }
+
+    def delete(seqNr: SeqNr) = snapshotter.deleteSnapshot(seqNr)
+
+    def delete(criteria: SnapshotSelectionCriteria) = snapshotter.deleteSnapshots(criteria)
   }
 }
