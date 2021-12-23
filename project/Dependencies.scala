26f7697ae63bfa72600454398d1d8343e25dfebe
Scala Steward <me@scala-steward.org>
2021-12-23 17:00:51 +0100

    Update logback-classic to 1.2.10

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -24,7 +24,7 @@ object Dependencies {
   }
 
   val scalatest                   = "org.scalatest"       %% "scalatest"                 % "3.2.8"
-  val `logback-classic`           = "ch.qos.logback"       % "logback-classic"           % "1.2.3"
+  val `logback-classic`           = "ch.qos.logback"       % "logback-classic"           % "1.2.10"
   val `akka-persistence-inmemory` = "com.github.dnvriend" %% "akka-persistence-inmemory" % "2.5.15.2"
   val `executor-tools`            = "com.evolutiongaming" %% "executor-tools"            % "1.0.2"
   val nel                         = "com.evolutiongaming" %% "nel"                       % "1.3.4"
