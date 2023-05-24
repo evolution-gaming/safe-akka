854de5220d5206e1ea577ca0c7e7dd3ce46e5f51
Scala Steward <scala_steward@virtuslab.com>
2023-05-24 20:05:03 +0000

    Update nel to 1.3.5

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -27,5 +27,5 @@ object Dependencies {
   val `logback-classic`           = "ch.qos.logback"       % "logback-classic"           % "1.2.3"
   val `akka-persistence-inmemory` = "com.github.dnvriend" %% "akka-persistence-inmemory" % "2.5.15.2"
   val `executor-tools`            = "com.evolutiongaming" %% "executor-tools"            % "1.0.2"
-  val nel                         = "com.evolutiongaming" %% "nel"                       % "1.3.4"
+  val nel                         = "com.evolutiongaming" %% "nel"                       % "1.3.5"
 }
