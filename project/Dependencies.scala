3c53a628178d131ee29c76fa5b21e9ea54f9e027
Scala Steward <scala_steward@virtuslab.com>
2024-10-08 15:13:52 +0000

    Update logback-classic to 1.5.9

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -24,6 +24,6 @@ object Dependencies {
   }
 
   val scalatest                   = "org.scalatest"       %% "scalatest"                 % "3.2.19"
-  val `logback-classic`           = "ch.qos.logback"       % "logback-classic"           % "1.5.7"
+  val `logback-classic`           = "ch.qos.logback"       % "logback-classic"           % "1.5.9"
   val nel                         = "com.evolutiongaming" %% "nel"                       % "1.3.5"
 }
