a072dc1a1c51ec64ed7b79a2b57464b7d7b43da9
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2026-02-11 07:35:47 +0100

    Update logback-classic to 1.5.29 (#205)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -24,6 +24,6 @@ object Dependencies {
   }
 
   val scalatest                   = "org.scalatest"       %% "scalatest"                 % "3.2.19"
-  val `logback-classic`           = "ch.qos.logback"       % "logback-classic"           % "1.5.28"
+  val `logback-classic`           = "ch.qos.logback"       % "logback-classic"           % "1.5.29"
   val nel                         = "com.evolutiongaming" %% "nel"                       % "1.3.5"
 }
